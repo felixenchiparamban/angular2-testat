b054d4cadc34cb2a34f6f9465a1a59bdcd2f1ed6
tk-codes <kk.devpro11@gmail.com>
2017-03-17 11:59:43 +0100

    dashboard component

--- frontend/src/app/dashboard/dashboard.module.ts
+++ frontend/src/app/dashboard/dashboard.module.ts
@@ -3,17 +3,19 @@ import {NgModule, ModuleWithProviders} from '@angular/core';
 import {SharedModule} from "../shared/shared.module";
 
 import {DashbaordRoutingModule} from "./dashboard-routing.module";
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     // Declarations (Components / Directives) used from/within the Module
-  ],
+  DashboardComponent],
   imports: [
     // Other Modules to import (imports the exported Components/Directives from the other module)
     SharedModule, DashbaordRoutingModule
   ],
   exports: [
     // Components/Directives (or even Modules) to export (available for other modules; and forRoot() )
+    DashboardComponent
   ],
   providers: [
     // DI Providers (Services, Tokens, Factories...), may be instantiated multiple times
