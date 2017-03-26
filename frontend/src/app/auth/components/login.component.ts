c0a434b7801fe34c9e2bd5e12122dbc712ee03f9
Felix Varghese <felixenchiparambil@hotmail.com>
2017-03-26 11:55:13 +0200

    auth mod.

--- frontend/src/app/auth/components/login.component.ts
+++ frontend/src/app/auth/components/login.component.ts
@@ -1,6 +1,7 @@
 import {ActivatedRoute, Params} from "@angular/router";
 import {Component, OnInit} from '@angular/core';
 import {NgForm} from "@angular/forms";
+import { RouterModule } from "@angular/router";
 
 import {NavigationService} from "../../core/services/navigation.service";
 
