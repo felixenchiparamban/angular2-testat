63e5740b576ac53ecb80ca7dcf03a24fc8f6c3e8
tk-codes <kk.devpro11@gmail.com>
2017-04-02 14:21:51 +0200

    transfer payment

--- frontend/src/app/dashboard/pipes/currencydisplay.pipe.ts
+++ frontend/src/app/dashboard/pipes/currencydisplay.pipe.ts
@@ -6,7 +6,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class CurrencydisplayPipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
+    if(!value){
+      return '';
+    }
     return value.toFixed(2) + " CHF";
   }
-
 }
